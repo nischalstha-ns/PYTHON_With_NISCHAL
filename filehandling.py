78a9794d4df7eb46d21170a28bcf307d74b451a2
nischalstha-ns <niscthastha123@gmail.com>
2025-11-10 20:08:25 +0545

    3

--- filehandling.py
+++ filehandling.py
@@ -1,4 +1,16 @@
+# f= open('myfile.txt','r')
+# text=f.read()
+# print(text)
+# f.close()
+
+
+# writing to the file
+f= open('myfile.txt','w')
+f.write("hello my name is nischal shrestha")
+f.close()
+
 f= open('myfile.txt','r')
 text=f.read()
 print(text)
 f.close()
+
