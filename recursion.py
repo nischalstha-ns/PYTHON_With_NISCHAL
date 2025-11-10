7e1ab4b3e958a4f9959a8faef4286ccc24538749
nischalstha-ns <niscthastha123@gmail.com>
2025-11-10 20:05:04 +0545

    3

--- recursion.py
+++ recursion.py
@@ -5,12 +5,38 @@
 #     fact = fact * i
 # print(fact)
 
-def trirecursive(k):
-    if k>0:
-        result=k*trirecursive(k-1)
-        print(result)
+# def trirecursive(k):
+#     if k>0:
+#         result=k*trirecursive(k-1)
+#         print(result)
 
-    else:
-        result=0
-k=int(input("enter the valur"))
-trirecursive(k)
+#     else:
+#         result=0
+# k=int(input("enter the valur"))
+# trirecursive(k)
+
+# the function useing itself is called  recurisive
+# def fact(n):
+#     if n==0 or n==1:
+#         return 1
+#     else:
+#         return n*fact(n-1)
+#         print(fact )
+# print(fact(5))  
+# print(fact(4))  
+# print(fact(5))  
+# print(fact(1))  
+
+# print 1
+# 1*1=1
+# 1*2=2
+# 1*3=3
+# |
+# |
+# 1*10=10
+
+# def Multi(l):
+#     for i in range(1,11):
+        
+#         print(l, "*", i ,"=" ,i*l)
+# Multi(2)     
